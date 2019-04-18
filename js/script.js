988371abe6156635e435f1b766cb09482e6d78bc
Maksim <mvbaulin@yandex.ru>
2019-04-18 17:35:00 +0300

    added calculator script open/close

--- js/script.js
+++ js/script.js
@@ -2,8 +2,9 @@ var burger = document.querySelector('.site-menu__burger');
 var mainNav = document.querySelector('.site-menu__navigation');
 
 function noScript(){
-	mainNav.classList.remove('site-menu__navigation--nojs');
-	// сделать проверку
+	if (mainNav.classList.contains('site-menu__navigation--nojs')) {
+		mainNav.classList.remove('site-menu__navigation--nojs');
+	}
 }
 noScript();
 
@@ -11,5 +12,23 @@ noScript();
 burger.addEventListener('click', openCloseMenu);
 function openCloseMenu(){
 	mainNav.classList.toggle('site-menu__navigation--opened');
-	console.log('sfsaf');
+}
+
+var calculatorButton = document.querySelectorAll('.calculator__checkbox');
+var calculatorHiddenAreas = document.querySelectorAll('.calculator__list--hidden');
+
+for (var i = 0; i < calculatorHiddenAreas.length; i++) {
+	calculatorHiddenAreas[i];
+}
+
+for (let i = 0; i < calculatorButton.length; i++) {
+	calculatorButton[i].addEventListener('change', function(){
+		if (calculatorButton[i].checked) {
+			calculatorHiddenAreas[i].classList.remove('calculator__list--hidden');
+			console.log('checked button' + i);
+		} else {
+			calculatorHiddenAreas[i].classList.add('calculator__list--hidden');
+			console.log('unchecked button' + i);
+		}
+	});
 }
